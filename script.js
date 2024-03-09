b45682ef5a94dd87eaed72cec8fe579b60519ae7
Yankho Chantel Maria Saliji <ysaliji023@student.wethinkcode.co.za>
2024-03-09 14:49:56 +0200

    added stuling for the link and the button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// JavaScript validation for password matching
-const form = document.getElementById('signup-form');
+// // JavaScript validation for password matching
+// const form = document.getElementById('signup-form');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-    const password = form.querySelector('input[name="password"]').value;
-    const confirm_password = form.querySelector('input[name="confirm_password"]').value;
+//     const password = form.querySelector('input[name="password"]').value;
+//     const confirm_password = form.querySelector('input[name="confirm_password"]').value;
 
-    if (password !== confirm_password) {
-        alert("Passwords do not match!");
-        return;
-    }
+//     if (password !== confirm_password) {
+//         alert("Passwords do not match!");
+//         return;
+//     }
 
-    // Submit the form if passwords match
-    form.submit();
-});
+//     // Submit the form if passwords match
+//     form.submit();
+// });
